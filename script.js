481379a84bbf84d9d99fd7246ec1c9c62f01a3ee
Delgorithm <delgorithm@gmail.com>
2023-04-26 16:15:30 +0200

    JS : Use of the API

---
+++ script.js
@@ -0,0 +1,28 @@
+const adviceCapt = document.querySelector('#caption-advice');
+const diceBtn = document.querySelector('#icon-dice');
+
+const numberAdv = document.querySelector('#number-advice');
+
+
+diceBtn.addEventListener('click', () => {
+    fetch('https://api.adviceslip.com/advice').then(Response => {
+        return Response.json();
+    })
+    .then(adviceData => {
+
+        // Code for the "advice displayed"
+        const AdviceObj = adviceData.slip;
+        adviceCapt.innerHTML = `${AdviceObj.advice}`;
+
+
+        // Code for the "Advice #"
+        const AdviceNbr = adviceData.slip;
+        numberAdv.innerHTML = `Advice # ${AdviceNbr.id}`
+        
+        
+        console.log(AdviceObj);
+    })
+    .catch(error => {
+        console.log(error)
+    })
+});
